90eda62a601f2fc4b1b3b57e8e5e6838fd5e9297
Koom <m.garnier.mickael@gmail.com>
2022-03-06 15:19:12 +0100

    Refactored cpu::register::Status so that it is initialized according to specs

--- src/cpu/register.rs
+++ src/cpu/register.rs
@@ -6,14 +6,19 @@ pub type X = u8;
 pub type Y = u8;
 pub type StackPointer = u8;
 
+const PROGRAM_COUNTER_START: ProgramCounter = 0xFFFC;
+
 bitflags! {
     pub struct ProcFlag: u8 {
-        const NEGATIVE = 0b1000_0000;
-        const OVERFLOW = 0b0100_0000;
-        const DECIMAL = 0b0000_1000;
-        const INTERRUPT_DISABLE = 0b0000_0100;
-        const ZERO = 0b0000_0010;
-        const CARRY = 0b0000_0001;
+        const NEGATIVE =            0b1000_0000;
+        const OVERFLOW =            0b0100_0000;
+        const UNUSED =              0b0010_0000;
+        const BREAK =               0b0001_0000;
+        const DECIMAL =             0b0000_1000;
+        const INTERRUPT_DISABLE =   0b0000_0100;
+        const ZERO =                0b0000_0010;
+        const CARRY =               0b0000_0001;
+        const INITIAL_STATE = Self::INTERRUPT_DISABLE.bits() | Self::UNUSED.bits();
     }
 }
 
@@ -25,7 +30,7 @@ pub struct Status {
 impl Status {
     pub fn new() -> Status {
         Status {
-            status: ProcFlag::empty(),
+            status: ProcFlag::INITIAL_STATE,
         }
     }
 
@@ -72,21 +77,18 @@ mod tests {
         status |= ProcFlag::DECIMAL;
         assert!(status.is_set(ProcFlag::DECIMAL));
         assert!(!status.is_unset(ProcFlag::DECIMAL));
-        assert_eq!(status, 0b0000_1000);
-        assert_eq!(status, ProcFlag::DECIMAL);
+        assert_eq!(status, ProcFlag::INITIAL_STATE | ProcFlag::DECIMAL);
     }
 
     #[test]
     fn unset_a_flag() {
         let mut status = Status::new();
         status |= ProcFlag::DECIMAL;
-        assert_eq!(status, 0b0000_1000);
-        assert_eq!(status, ProcFlag::DECIMAL);
+        assert_eq!(status, ProcFlag::INITIAL_STATE | ProcFlag::DECIMAL);
         status &= !ProcFlag::DECIMAL;
         assert!(!status.is_set(ProcFlag::DECIMAL));
         assert!(status.is_unset(ProcFlag::DECIMAL));
-        assert_eq!(status, 0b0000_0000);
-        assert_eq!(status, ProcFlag::empty());
+        assert_eq!(status, ProcFlag::INITIAL_STATE);
     }
 
     #[test]
@@ -101,10 +103,9 @@ mod tests {
         assert!(!status.is_unset(ProcFlag::CARRY));
         assert!(!status.is_unset(ProcFlag::NEGATIVE));
         assert!(!status.is_unset(ProcFlag::DECIMAL | ProcFlag::CARRY | ProcFlag::NEGATIVE));
-        assert_eq!(status, 0b1000_1001);
         assert_eq!(
             status,
-            ProcFlag::DECIMAL | ProcFlag::CARRY | ProcFlag::NEGATIVE
+            ProcFlag::INITIAL_STATE | ProcFlag::DECIMAL | ProcFlag::CARRY | ProcFlag::NEGATIVE
         );
     }
 
@@ -113,16 +114,14 @@ mod tests {
         let mut status = Status::new();
         status |=
             ProcFlag::OVERFLOW | ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO | ProcFlag::DECIMAL;
-        assert_eq!(status, 0b0100_1110);
         assert_eq!(
             status,
-            ProcFlag::OVERFLOW | ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO | ProcFlag::DECIMAL
+            ProcFlag::INITIAL_STATE | ProcFlag::OVERFLOW | ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO | ProcFlag::DECIMAL
         );
         status &= !(ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO);
         assert!(status.is_set(ProcFlag::OVERFLOW | ProcFlag::DECIMAL));
         assert!(status.is_unset(ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO | ProcFlag::NEGATIVE));
-        assert_eq!(status, 0b0100_1000);
-        assert_eq!(status, ProcFlag::OVERFLOW | ProcFlag::DECIMAL);
+        assert_eq!(status, ProcFlag::UNUSED | ProcFlag::OVERFLOW | ProcFlag::DECIMAL);
     }
 
     #[test]
@@ -133,6 +132,6 @@ mod tests {
         assert!(status.is_set(ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO));
         assert!(status.is_set(ProcFlag::CARRY | ProcFlag::NEGATIVE));
         assert!(status.is_unset(ProcFlag::DECIMAL | ProcFlag::OVERFLOW));
-        assert_eq!(status, 0b1000_0111)
+        assert_eq!(status, ProcFlag::INITIAL_STATE | ProcFlag::INTERRUPT_DISABLE | ProcFlag::ZERO | ProcFlag::CARRY | ProcFlag::NEGATIVE)
     }
 }
