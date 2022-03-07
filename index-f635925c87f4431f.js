218fed565bd0e1f2a701995f969ae001e44c1db8
wiktorwieclaw <wiktorwieclaw@users.noreply.github.com>
2022-03-07 14:19:13 +0000

    deploy: 5efac06e313d4368ead6569bad7c59fcd3188f49

--- index-f635925c87f4431f.js
+++ index-f635925c87f4431f.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b3c1e10667c3de66_bg.wasm', import.meta.url);
+        input = new URL('index-f635925c87f4431f_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
